aa6320bd11c45102f7977e1ce4ad0079e188065a
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-01 14:04:16 -0500

    Add resizing nav on large screens on scroll

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-window.onscroll=function(e) {
-    console.log(this.oldScroll>this.scrollY);
-    this.oldScroll = this.scrollY;
-    console.log("hello!");
-}
+let subscribeBanner = document.querySelector(".subscribe-banner-wrapper");
+let navFull = document.querySelectorAll("nav, .nav-links-container ul");
+let logoImage = document.querySelectorAll(
+  ".header-png-container img",
+  ".header-png-container"
+);
 
-console.log("goodbye!");
+window.onscroll = function (e) {
+  console.log(this.oldScroll < this.scrollY);
+
+  if (!(this.oldScroll > this.scrollY)) {
+    // console.log("scroll down");
+
+    subscribeBanner.classList.add("opacity-zero");
+    navFull.forEach((element) => element.classList.add("shrink-nav-ul"));
+    logoImage.forEach((element) => element.classList.add("shrink-logo"));
+  }
+
+  if (this.oldScroll > this.scrollY) {
+    subscribeBanner.classList.remove("opacity-zero");
+    navFull.forEach((element) => element.classList.remove("shrink-nav-ul"));
+    logoImage.forEach((element) => element.classList.remove("shrink-logo"));
+  }
+
+  this.oldScroll = this.scrollY;
+};
